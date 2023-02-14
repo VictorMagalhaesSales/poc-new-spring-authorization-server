9e954fe877b6640add32485cb32a90d81d544506
Victor Magalhães <victormagalhaessales@gmail.com>
2023-02-14 09:03:30 -0300

    Init spring boot project with web and security

---
+++ src/main/java/com/poc/authorizationserver/PocSpringAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.poc.authorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSpringAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSpringAuthorizationServerApplication.class, args);
+	}
+
+}
