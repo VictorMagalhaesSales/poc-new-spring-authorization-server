c84b0b4da474ef6e41922c8e4391a4ce3dde4996
Victor Magalhães <victormagalhaessales@gmail.com>
2023-02-25 16:41:19 -0300

    Create resource server config with reference to AuthServer

--- resource-server/src/main/java/com/poc/resourceserver/security/BasicSecurityConfig.java
+++ resource-server/src/main/java/com/poc/resourceserver/security/BasicSecurityConfig.java
@@ -2,9 +2,9 @@ package com.poc.resourceserver.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,11 +13,14 @@ public class BasicSecurityConfig {
 	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf().disable();
 		
 		http.authorizeHttpRequests().anyRequest().authenticated();
 		
-		http.formLogin(Customizer.withDefaults());
+		http.csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		
+		http.oauth2ResourceServer().jwt();
+		
 		
 		return http.build();
 	}
