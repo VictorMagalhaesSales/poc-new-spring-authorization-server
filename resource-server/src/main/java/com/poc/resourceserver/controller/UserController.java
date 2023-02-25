9494f8af0b892a5c0f7c6d8c0809cd4c0591f846
Victor Magalhães <victormagalhaessales@gmail.com>
2023-02-25 10:15:45 -0300

    Implements basic authorization with cookie session on ResourceServer

--- resource-server/src/main/java/com/poc/resourceserver/controller/UserController.java
+++ resource-server/src/main/java/com/poc/resourceserver/controller/UserController.java
@@ -1,4 +1,4 @@
-package com.poc.authorizationserver.controller;
+package com.poc.resourceserver.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
